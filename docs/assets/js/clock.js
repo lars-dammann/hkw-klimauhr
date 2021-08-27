cec82214a251f461041a942fb4d2894f97f3d529
Lars Dammann <lars_dammann@hotmail.de>
2021-08-27 12:48:16 +0200

    Deengeniered countdown script

--- docs/assets/js/clock.js
+++ docs/assets/js/clock.js
@@ -1,26 +1,23 @@
-const svg = d3.select("#clock_area").append("svg")
-  .attr("width", 400)
-  .attr("height", 100)
+function countdown() {
+  var countDownDate = new Date("Jul 24, 2027 16:00:00").getTime();
 
-var countDownDate = new Date("Jul 24, 2027 16:00:00").getTime();
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-d3.interval(function(){
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Time calculations for days, hours, minutes and seconds
-    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
-    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    const text = ""+years + " Jahre"+ days + " Tage " + hours + " Stunden "+ minutes + " Minuten"
-    svg.selectAll("text").remove()
+  document.getElementById("years").innerHTML = years
+  document.getElementById("days").innerHTML = days
+  document.getElementById("hours").innerHTML = hours
+  document.getElementById("minutes").innerHTML = minutes
+  document.getElementById("seconds").innerHTML = seconds
 
-    svg
-    .append("text")
-    .attr("x",0).attr("y",50).attr("font-size","22px").text(text)
-    .attr("fill","orange")
 
-},1000);
+};
+
+setInterval(countdown, 1000);
